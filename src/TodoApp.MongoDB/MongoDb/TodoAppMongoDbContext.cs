e5764ed9090b579f675b49129c132a9bc23e0bcc
datnguyennt <datnguyennguyen.2000@gmail.com>
2021-07-20 17:03:46 +0700

    Add project files.

---
+++ src/TodoApp.MongoDB/MongoDb/TodoAppMongoDbContext.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using TodoApp.Users;
+using Volo.Abp.Data;
+using Volo.Abp.MongoDB;
+
+namespace TodoApp.MongoDB
+{
+    [ConnectionStringName("Default")]
+    public class TodoAppMongoDbContext : AbpMongoDbContext
+    {
+        public IMongoCollection<TodoItem> TodoItems => Collection<TodoItem>();
+
+        public IMongoCollection<AppUser> Users => Collection<AppUser>();
+
+        protected override void CreateModel(IMongoModelBuilder modelBuilder)
+        {
+            base.CreateModel(modelBuilder);
+
+            modelBuilder.Entity<TodoItem>(b =>
+            {
+                b.CollectionName = "TodoItems";
+            });
+
+
+            modelBuilder.Entity<AppUser>(b =>
+            {
+                /* Sharing the same "AbpUsers" collection
+                 * with the Identity module's IdentityUser class. */
+                b.CollectionName = "AbpUsers";
+            });
+        }
+    }
+}
